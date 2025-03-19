62e73d41d622d3be0d47bd6ccd09f5ecc5ebc5b5
Pedro Henrique Bueno Vasconcellos <138713757+pedrobvasconcellos@users.noreply.github.com>
2025-03-19 08:28:58 -0300

    Delete Aula 7 directory

--- Aula 7/projeto1/backend/db/conn.js
+++
@@ -1,9 +0,0 @@
-//7//
-import mongoose from 'mongoose';
-async function main(){
-    await mongoose.connect('mongodb://localhost:27017/projeto1')
-    console.log('Conectou Mongoose'); 
-}
-main().catch((err) => console.log(err));
-export default mongoose;
-////
